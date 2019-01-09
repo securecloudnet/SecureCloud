1f80e7fed8734321b2ca9002eab92841b228c936
ZioFabry <ziofabry@hotmail.com>
2019-01-09 22:01:06 +0100

    Future protocol enforcement implemented

--- src/main.cpp
+++ src/main.cpp
@@ -5276,8 +5276,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         masternodeSync.ProcessMessage(pfrom, strCommand, vRecv);
     }
 
-
-    return true;
+	return true;
 }
 
 // Note: whenever a protocol update is needed toggle between both implementations (comment out the formerly active one)
@@ -5286,19 +5285,21 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
+    if (IsSporkActive(SPORK_20_NEW_PROTOCOL_ENFORCEMENT_6))
+        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_6;
 
-    // SPORK_14 was used for 70910. Leave it 'ON' so they don't see > 70910 nodes. They won't react to SPORK_15
-    // messages because it's not in their code
+    if (IsSporkActive(SPORK_19_NEW_PROTOCOL_ENFORCEMENT_5))
+        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_5;
 
-/*    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
-            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
-*/
+    if (IsSporkActive(SPORK_18_NEW_PROTOCOL_ENFORCEMENT_4))
+        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_4;
 
-    // SPORK_15 is used for 70911. Nodes < 70911 don't see it and still get their protocol version via SPORK_14 and their
-    // own ModifierUpgradeBlock()
+    if (IsSporkActive(SPORK_17_NEW_PROTOCOL_ENFORCEMENT_3))
+        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3;
 
     if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
-            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
+        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2;
+
     return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
 }
 
